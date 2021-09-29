3ccfa3769c13dd20a0736119c248f2c522f31b73
Dan Smestad <hockeydan25@gmail.com>
2021-09-29 01:04:43 -0500

    hello canvas html added

---
+++ quiz.js
@@ -0,0 +1,14 @@
+//code run for quiz this week
+
+let text = 'kittens' 
+let indexOfZ = text.indexOf('z') //index is not here so -1. if k =0 or s = 6.
+console.log(indexOfZ)
+
+let trees = ['oak', 'elm', 'ash'] // trees is only three in a array here .push adds maple.
+trees.push('maple')
+console.log(trees)
+
+//can I alter this or learn to use it? 
+// array.forEach(element => {
+    
+// });
